489eda91250835bf6caaac9793c959e2fa95f818
donkeysharp <serguimant@gmail.com>
2014-05-17 16:46:33 -0400

    First commit for pizarron, yeah!

---
+++ app/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function() {
+    $a = new Person();
+    $a->show();
+	return View::make('hello');
+});
---
+++ app/views/error.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+  <title>Error</title>
+</head>
+<body>
+  <h1>Just got a fucking error!!!! :S</h1>
+</body>
+</html>
