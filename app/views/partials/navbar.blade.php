5ca4a83296e8c157db54929ccd605bb284c80c4c
donkeysharp <serguimant@gmail.com>
2014-05-25 00:07:18 -0400

    Fixed navbar style bugs, also it is now generic so it can be exapanded easily ... thx bootstrap :D

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -10,7 +10,7 @@
 
   <div class="collapse navbar-collapse" id="my-navbar">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">+</a></li>
+      <li><a href="#">pr0n</a></li>
     </ul>
     <ul class="nav navbar-nav navbar-right">
       @if(!Auth::check())
